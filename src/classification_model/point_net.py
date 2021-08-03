96ddbe416e706fa4ea84db014d1f2efa7dd23f56
Anzii <luoshunan1994@gmail.com>
2021-08-04 00:39:52 +0800

    增加pointnet模型

---
+++ src/classification_model/point_net.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project: point_set_platform 
+@Author: anzii.Luo
+@Describe: 
+@Date: 2021/8/3
+"""
+import tensorflow as tf
+
+
+class PointNet:
+    def __index__(self):
+        pass
