7d48bd8c67280d0bf8e29a2549f17cbe62bf1862
anzii <luoshunan1994@gmail.com>
2021-08-05 19:42:08 +0800

    增加pointnet

--- src/classification_model/point_net.py
+++ src/classification_model/point_net.py
@@ -10,5 +10,37 @@ import tensorflow as tf
 
 
 class PointNet:
-    def __index__(self):
+    def __init__(self, batch_size, num_point):
+        """
+        模型初始化
+        :param batch_size:
+        :param num_point:
+        """
+        self.batch_size = batch_size
+        self.num_point = num_point
+        self.point_pl = tf.placeholder(
+            dtype=tf.float32, shape=(batch_size, num_point, 3)
+        )
+        self.label_pl = tf.placeholder(dtype=tf.int32, shape=batch_size)
+
+    def get_placeholder(self):
+        """
+        获取输入占位符
+        :return:
+        """
+        return self.point_pl, self.label_pl
+
+    def get_model(self):
+        """
+        获取模型
+        :return:
+        """
         pass
+
+    def _input_transform_net(self):
+        """
+        输入转换网络，是一个多层卷积网络
+        :return:
+        """
+        # 增加一维以进行卷积运算
+        input_data = tf.expand_dims(input=self.point_pl, axis=-1)
---
+++ src/utils/tf_tools.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project: point_set_platform 
+@Author: anzii.Luo
+@Describe: 一些构建网络的工具
+@Date: 2021/8/4
+"""
+import tensorflow as tf
+
+def point_conv2d(input_data, num_output_channels, kernel_size)
